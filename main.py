91092f598d6517ec0f3cea5d673e9d7a9fe24fd1
Artur Ferreira Moreira <artur31415926@gmail.com>
2022-03-10 11:58:19 -0300

    path code

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ pygame.init()
 pygame.font.init()
 
 # seed random number generator
-seed(1)
+#seed(1)
 ################################################################################################
 #                                           VARIABLES
 ################################################################################################
@@ -29,7 +29,7 @@ grid_height = 70
 
 cell_size = 10
 
-current_path = [(1, 1)]
+current_path = [(35, 35)]
 
 clock = pygame.time.Clock()
 ################################################################################################
@@ -56,16 +56,32 @@ def draw_grid(DISPLAY):
             pixel = 255 * (1 - grid[i][j])
             pygame.draw.rect(DISPLAY, (pixel, pixel, pixel), (i * cell_size, j * cell_size, cell_size, cell_size))
 
+    for i in range(grid_width):
+        s_v_pos = (i * cell_size, 0)
+        e_v_pos = (i * cell_size, height)
+
+        s_h_pos = (0, i * cell_size)
+        e_h_pos = (width, i * cell_size)
+
+        pygame.draw.line(DISPLAY, (0, 0, 0), s_v_pos, e_v_pos)
+        pygame.draw.line(DISPLAY, (0, 0, 0), s_h_pos, e_h_pos)
+
 def draw_path(DISPLAY, path):
     last_node = None
     for path_node in path:
+        path_node_color = (255, 0, 0)
+        path_node_radius = cell_size / 4
         path_node_cartesian = grid_to_cartesian(path_node, cell_size)
+        path_node_cartesian = (path_node_cartesian[0] + cell_size / 2, path_node_cartesian[1] + cell_size / 2)
         #last_node_cartesian = grid_to_cartesian(path_node, cell_size)
         if last_node != None:
-            pygame.draw.line(DISPLAY, (255, 0, 0), last_node, path_node_cartesian, 2)
+            pygame.draw.line(DISPLAY, path_node_color, last_node, path_node_cartesian, 2)
+        else:
+            path_node_color = (0, 0, 255)
+            path_node_radius = cell_size / 2
         last_node = path_node_cartesian
 
-        pygame.draw.circle(DISPLAY, (255, 0, 0), path_node_cartesian, cell_size / 4)
+        pygame.draw.circle(DISPLAY, path_node_color, path_node_cartesian, path_node_radius)
 
 def is_node_free(path_node, world_grid):
     return world_grid[int(path_node[0])][int(path_node[1])] == 0
@@ -90,7 +106,7 @@ while running:
         new_node_cartesian = grid_to_cartesian(new_node, cell_size)
         if new_node_cartesian[0] >= 0 and new_node_cartesian[0] <= width and new_node_cartesian[1] >= 0 and new_node_cartesian[1] <= height and not (new_node in current_path) and is_node_free(new_node, grid):
             current_path.append(new_node)
-            print("new_node = ", str(new_node))
+            #print("new_node = ", str(new_node))
             break
 
     ##################################################################
